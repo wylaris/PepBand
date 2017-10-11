761269b19ff10c90033fc70a622c83cf9d3d290e
Scott Frauenknecht <32456160+Scott-Frauenknecht@users.noreply.github.com>
2017-10-11 09:39:27 -0400

    Logging setup for potential success

--- PepBandWebsite/IPCatch.py
+++ PepBandWebsite/IPCatch.py
@@ -1,12 +1,17 @@
 from ipware.ip import get_ip
-# import logging
+import logging
+
+logger=logging.getLogger('user_data')
 
 def IPCatcher(request):
     ip = get_ip(request)
     if ip is not None:
         print("We have an IP address for user")
         print(ip)
-        # logger=logging.getLogger("log_recording.txt")
+        logging.basicConfig(filename='log_recording.txt',
+                            level=logging.DEBUG,format='%(asctime)s %(message)s',
+                                                              datefmt='%m/%d/%Y %I:%M:%S %p')
+        logging.info('User data is being collected')
 
     else:
         print("we don't have an IP address for user")
