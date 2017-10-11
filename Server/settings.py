eebd85ff2e23cb19731b34a9681e4190c954ee9d
Scott Frauenknecht <saf6260@rit.edu>
2017-10-11 12:17:46 -0400

    Revert "y"
    This reverts commit 1e3ceb453e3cf32a1dc80fe296e1fd2680c1d68f.

--- Server/settings.py
+++ Server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'e55!^b3txxn1yg^ms#0l1&%!htol0e@@ozcvdaea*8a@-_vn@%'
+SECRET_KEY = ''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
